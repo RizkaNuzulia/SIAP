18453b608c8601a12b257abd2e66e8e8c1df8db7
Rizka Nuzulia <rizka_lia@mhs.unsyiah.ac.id>
2023-08-04 14:51:39 +0700

    btn

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
@@ -12,6 +14,7 @@
         <div class="atas">
             <img src="" alt="" class="logo">
             <h1 class="title">SIAP</h1>
+            <button class="msa">Masuk Sebagai Admin</button>
         </div>
     </nav>
 </body>
